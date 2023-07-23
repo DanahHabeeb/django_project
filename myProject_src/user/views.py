a9704af5e2e1efe585ee81e190c86edc9b0fb488
Danah Habeeb <danoosh-200@hotmail.com>
2023-07-23 22:10:30 +0300

    edit check before save data

---
+++ myProject_src/user/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .forms import UserCreationForm
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'user/register.html', {
+        'title': 'التسجيل',
+        'form': form,
+    })
+
